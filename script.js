51caf2e0467ba65dbcb63fc11fdbf52257c8b1d1
Jacinto Wong <contact@jacinto.design>
2023-11-09 21:04:31 -0500

    finalized contact section

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 5000); 
 
     // Random User Images -----------------------------
-    fetch('https://randomuser.me/api/?gender=female&results=3')
+    fetch('https://randomuser.me/api/?gender=male&results=3')
         .then(response => response.json())
         .then(data => {
             const testimonials = document.querySelectorAll('.testimonial');
@@ -27,29 +27,24 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('Error fetching user data:', error));
 
     // Banner -----------------------------------------
-    const bannerContent = document.querySelector('.banner-content');
-    const message = '<a href="mailto:email@example.com" class="contact-link">Contact us at email@example.com - We\'re here to help!</a>';
-    let messages = '';
+    const bannerContent = document.getElementById('bannerContent');
+    let messageHTML = '<span class="contact-message">Contact us at email@example.com - We\'re here to help!</span>';
+    let repeatedMessage = messageHTML.repeat(10); // Create a string with the message repeated 10 times
+    
+    // Set the repeated messages as the content
+    bannerContent.innerHTML = repeatedMessage + repeatedMessage; // Duplicate for infinite scrolling
   
-    // Generate 10 messages
-    for (let i = 0; i < 10; i++) {
-      messages += message;
+    function scrollBanner() {
+      if (bannerContent.scrollLeft >= bannerContent.firstChild.offsetWidth * 10) {
+        bannerContent.scrollLeft = 0; // Reset scroll position when end is reached
+      } else {
+        bannerContent.scrollLeft += 2; // Increase for a faster scroll speed
+      }
+      requestAnimationFrame(scrollBanner);
     }
   
-    bannerContent.innerHTML = messages + messages; // Duplicate the set of messages
-  
-    const scroll = () => {
-      // Reset the scroll position to 0 if it has reached the end of the first set of messages
-      if (bannerContent.offsetWidth + bannerContent.scrollLeft >= bannerContent.scrollWidth / 2) {
-        bannerContent.scrollLeft = 0;
-      } else {
-        bannerContent.scrollLeft += 1; // Adjust the speed as needed
-      }
-      window.requestAnimationFrame(scroll);
-    };
-  
-    scroll();
+    scrollBanner();
 });
 
-// Houdini
+// Houdini Worklet
 CSS.paintWorklet.addModule('https://unpkg.com/parallelowow@0.1.5/parallelowow.js');
