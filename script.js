1e4b148f03db83f2b10a19df37eae454d0ca5a47
Jacinto Wong <contact@jacinto.design>
2023-11-11 14:21:52 -0500

    add navigation, custom scrollbar

--- script.js
+++ script.js
@@ -48,3 +48,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Houdini Worklet
 CSS.paintWorklet.addModule('https://unpkg.com/parallelowow@0.1.5/parallelowow.js');
+
+// Navigation
+const nav = document.getElementById('nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+  nav.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+}
+
+function hideMenu() {
+  nav.classList.remove('active');
+  menuIcon.classList.remove('active');
+}
