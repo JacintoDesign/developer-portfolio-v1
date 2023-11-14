a0205a8c5e05e31a56f39aa0d825eba7478d0d1f
Jacinto Wong <contact@jacinto.design>
2023-11-14 09:46:08 -0500

    re-factor javascript

--- script.js
+++ script.js
@@ -12,40 +12,38 @@ function hideMenu() {
   menuIcon.classList.remove('active');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Slideshow ---------------------------------------
-  let slides = document.querySelectorAll('.slide');
-  let currentSlide = 0;
+// Slideshow ---------------------------------------
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
 
-  function goToSlide(n) {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (n+slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
+function goToSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (n+slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(() => {
+  goToSlide(currentSlide + 1);
+}, 5000); 
+
+// Banner ------------------------------------------
+const bannerContent = document.getElementById('bannerContent');
+let messageHTML = '<span class="contact-message">Contact us at email@example.com - We\'re here to help!</span>';
+let repeatedMessage = messageHTML.repeat(10); // Create a string with the message repeated 10 times
+
+// Set the repeated messages as the content
+bannerContent.innerHTML = repeatedMessage + repeatedMessage; // Duplicate for infinite scrolling
+
+function scrollBanner() {
+  if (bannerContent.scrollLeft >= bannerContent.firstChild.offsetWidth * 10) {
+    bannerContent.scrollLeft = 0; // Reset scroll position when end is reached
+  } else {
+    bannerContent.scrollLeft += 2; // Increase for a faster scroll speed
   }
+  requestAnimationFrame(scrollBanner);
+}
 
-  setInterval(() => {
-    goToSlide(currentSlide + 1);
-  }, 5000); 
-
-  // Banner ------------------------------------------
-  const bannerContent = document.getElementById('bannerContent');
-  let messageHTML = '<span class="contact-message">Contact us at email@example.com - We\'re here to help!</span>';
-  let repeatedMessage = messageHTML.repeat(10); // Create a string with the message repeated 10 times
-  
-  // Set the repeated messages as the content
-  bannerContent.innerHTML = repeatedMessage + repeatedMessage; // Duplicate for infinite scrolling
-
-  function scrollBanner() {
-    if (bannerContent.scrollLeft >= bannerContent.firstChild.offsetWidth * 10) {
-      bannerContent.scrollLeft = 0; // Reset scroll position when end is reached
-    } else {
-      bannerContent.scrollLeft += 2; // Increase for a faster scroll speed
-    }
-    requestAnimationFrame(scrollBanner);
-  }
-
-  scrollBanner();
-});
+scrollBanner();
 
 // Houdini Worklet
 CSS.paintWorklet.addModule('https://unpkg.com/parallelowow@0.1.5/parallelowow.js');
